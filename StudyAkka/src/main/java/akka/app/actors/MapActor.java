e58f2514427f8a87d944bb86d01c28eea33da87a
liguodong <liguodongiot@163.com>
2015-12-17 00:21:05 +0800

    Modify

---
+++ StudyAkka/src/main/java/akka/app/actors/MapActor.java
@@ -0,0 +1,7 @@
+package akka.app.actors;
+
+/**
+ * Created by liguodong on 2015/12/17.
+ */
+public class MapActor {
+}
---
+++ StudyAkka/src/main/java/akka/app/messages/HelloAkka.java
@@ -0,0 +1,8 @@
+package akka.app.messages;
+
+/**
+ * Created by liguodong on 2015/12/17.
+ */
+public class HelloAkka {
+
+}
